371382eaad2543873ead2ed65b38f2a53a4c3692
saidelsherbiny <79178690+saidelsherbiny@users.noreply.github.com>
2022-11-16 18:16:17 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php 
-$SERVER= 'wuc353.encs.concordia.ca:3306';
-$username='wuc353_1';
-$password='pro353sq';
-$database= 'wuc353_1';
+$SERVER= '*****************************';
+$username='******';
+$password='*******';
+$database= '*****';
 try{
     $conn =new PDO("mysql:host=$SERVER;dbname=$database;",$username, $password);
 
